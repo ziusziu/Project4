1efbffe104e87803ae133d1cb99a281bd41e66cd
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-04-28 22:13:06 -0700

    Initial Project

---
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/MainActivity.java
@@ -0,0 +1,13 @@
+package siu.example.com.headingout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
