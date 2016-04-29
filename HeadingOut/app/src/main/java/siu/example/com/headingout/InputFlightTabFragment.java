68973806f90d06dc5ea5e121611e6947fc87bb6f
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-04-29 16:22:49 -0700

    SlidingTabs: Added fragment pager adapter for input activity

---
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/InputFlightTabFragment.java
@@ -0,0 +1,7 @@
+package siu.example.com.headingout;
+
+/**
+ * Created by samsiu on 4/29/16.
+ */
+public class InputFlightTabFragment {
+}
---
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/InputTabsFragmentPagerAdapter.java
@@ -0,0 +1,34 @@
+package siu.example.com.headingout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by samsiu on 4/29/16.
+ */
+public class InputTabsFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[]{"FLIGHT", "HOTELS", "TRANSIT"};
+
+    public InputTabsFragmentPagerAdapter(FragmentManager fm, String[] tabTitles) {
+        super(fm);
+        tabTitles = tabTitles;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return InputFlightTabFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
