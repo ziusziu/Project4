d5f5e66bfb40933c4a573aa65f90f24bee411570
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-04-29 16:09:46 -0700

    SlidingTabs: initialzied views in input activity

--- HeadingOut/app/src/main/java/siu/example/com/headingout/InputActivity.java
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/InputActivity.java
@@ -1,13 +1,39 @@
 package siu.example.com.headingout;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class InputActivity extends AppCompatActivity {
 
+    public static String POSITION = "POSITION";
+    private static TabLayout mTabLayout;
+    private static ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        mViewPager = (ViewPager)findViewById(R.id.input_viewPager);
+        mViewPager.setAdapter();
+
+        mTabLayout = (TabLayout)findViewById(R.id.input_tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    // Save and restore last known tab position
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(POSITION, mTabLayout.getSelectedTabPosition());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mViewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
     }
 }
