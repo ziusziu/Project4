fc8cd112579aeabcf71b44c66b3b0d0ffd9b6ba5
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-04-28 23:15:18 -0700

    NavDrawer,ToolBar: Added Java and edited xml

--- HeadingOut/app/src/main/java/siu/example/com/headingout/MainActivity.java
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/MainActivity.java
@@ -1,13 +1,66 @@
 package siu.example.com.headingout;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private static Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+        setSupportActionBar(mToolBar);
+        initNavDrawer();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    private void initializeViews(){
+        mToolBar = (Toolbar)findViewById(R.id.main_toolBar);
+    }
+
+    private void initNavDrawer(){
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                mToolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        drawer.removeDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }else if (id == R.id.nav_home){
+            
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
