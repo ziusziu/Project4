77d14478f00e038006ca554aff37b3269bfaa17d
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-04-29 16:29:19 -0700

    SlidingTabs: Added Fragment

--- HeadingOut/app/src/main/java/siu/example/com/headingout/InputFlightTabFragment.java
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/InputFlightTabFragment.java
@@ -1,7 +1,43 @@
 package siu.example.com.headingout;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by samsiu on 4/29/16.
  */
-public class InputFlightTabFragment {
+public class InputFlightTabFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static InputFlightTabFragment newInstance(int page){
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        InputFlightTabFragment fragment = new InputFlightTabFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.input_tab_flight_fragment, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+
+
 }
