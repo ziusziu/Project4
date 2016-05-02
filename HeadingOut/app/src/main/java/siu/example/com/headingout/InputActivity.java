f55a1f5242d6cc9afd31ce784442977e687e487f
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-05-02 14:25:53 -0700

    InputActivity: Added ToolBar

--- HeadingOut/app/src/main/java/siu/example/com/headingout/InputActivity.java
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/InputActivity.java
@@ -1,12 +1,20 @@
 package siu.example.com.headingout;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class InputActivity extends AppCompatActivity {
+public class InputActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static Toolbar mToolBar;
     public static String POSITION = "POSITION";
     private static TabLayout mTabLayout;
     private static ViewPager mViewPager;
@@ -15,6 +23,13 @@ public class InputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+
+
+        mToolBar = (Toolbar)findViewById(R.id.input_toolBar);
+
+        setSupportActionBar(mToolBar);
+        initNavDrawer();
 
         mViewPager = (ViewPager)findViewById(R.id.input_viewPager);
         mViewPager.setAdapter(new InputTabsFragmentPagerAdapter(getSupportFragmentManager()));
@@ -35,5 +50,50 @@ public class InputActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         mViewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    private void initNavDrawer(){
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.input_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                mToolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        drawer.removeDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.input_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.input_drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }else if (id == R.id.nav_home){
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.input_drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
