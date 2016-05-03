672343e595c3b82b085c916e7859db0a15e2db39
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-05-02 17:07:00 -0700

    DetailActivity: Added Toolbar Back Arrow and Temp PageViewer

--- HeadingOut/app/src/main/java/siu/example/com/headingout/DetailActivity.java
+++ HeadingOut/app/src/main/java/siu/example/com/headingout/DetailActivity.java
@@ -1,13 +1,54 @@
 package siu.example.com.headingout;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static Toolbar mToolBar;
+    public static String POSITION = "POSITION";
+    private static TabLayout mTabLayout;
+    private static ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mToolBar = (Toolbar)findViewById(R.id.detail_toolBar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("Detail");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mViewPager = (ViewPager)findViewById(R.id.detail_viewPager);
+        mViewPager.setAdapter(new InputTabsFragmentPagerAdapter(getSupportFragmentManager()));
+
+        mTabLayout = (TabLayout)findViewById(R.id.detail_tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
     }
+
+    // Save and restore last known tab position
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(POSITION, mTabLayout.getSelectedTabPosition());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mViewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
